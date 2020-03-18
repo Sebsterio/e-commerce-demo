1ef36a2b634fdd7ca7de8a3094df2b41d1aeebea
Sebsterio <sebastian.rosloniec@gmail.com>
2020-03-18 14:01:18 +0100

    implement code splitting; add error boundry component

--- client/src/App.js
+++ client/src/App.js
@@ -4,37 +4,47 @@ import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 import { Redirect } from "react-router-dom";
 
-import "./App.css";
-
-// import HomePage from "./pages/homepage/homepage.component";
-import ShopPage from "./pages/shop/shop.component";
-import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
-import CheckoutPage from "./pages/checkout/checkout.component";
 import Header from "./components/header/header.component";
+import Spinner from "./components/spinner/spinner.component";
+import ErrorBoundry from "./components/error-boundry/error-boundry.component";
 
 import { selectCurrentUser } from "./redux/user/user.selectors";
 import { checkUserSession } from "./redux/user/user.actions";
 
+import "./App.css";
+
+// lazy imports
 const HomePage = lazy(() => import("./pages/homepage/homepage.component"));
+const ShopPage = lazy(() => import("./pages/shop/shop.component"));
+const CheckoutPage = lazy(() =>
+	import("./pages/sign-in-and-sign-up/sign-in-and-sign-up.component")
+);
+const SignInAndSignUpPage = lazy(() =>
+	import("./pages/checkout/checkout.component")
+);
 
+// Component
 const App = ({ checkUserSession, currentUser }) => {
-	useEffect(() => checkUserSession(), [checkUserSession]);
-
+	useEffect(() => {
+		checkUserSession();
+	}, [checkUserSession]);
 	return (
 		<div>
 			<Header />
 			<Switch>
-				<Suspense fallback={<div>Loading...</div>}>
-					<Route exact path="/" component={HomePage} />
-				</Suspense>
-				<Route path="/shop" component={ShopPage} />
-				<Route path="/checkout" component={CheckoutPage} />
-				<Route
-					path="/signin"
-					render={() =>
-						currentUser ? <Redirect to="/" /> : <SignInAndSignUpPage />
-					}
-				/>
+				<ErrorBoundry>
+					<Suspense fallback={<Spinner />}>
+						<Route exact path="/" component={HomePage} />
+						<Route path="/shop" component={ShopPage} />
+						<Route path="/checkout" component={CheckoutPage} />
+						<Route
+							path="/signin"
+							render={() =>
+								currentUser ? <Redirect to="/" /> : <SignInAndSignUpPage />
+							}
+						/>
+					</Suspense>
+				</ErrorBoundry>
 			</Switch>
 		</div>
 	);
